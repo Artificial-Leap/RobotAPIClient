8eb1419d725df51dab6174f2d2258099dfd75a78
alextitonis <alexandrostitonis@gmail.com>
2023-04-22 10:20:48 +0300

    updates

--- imgClient.py
+++ imgClient.py
@@ -1,36 +1,35 @@
 import cv2
 import socket
 import pickle
-import os
-import numpy as np
+
+def initImgClient():
+    s = socket.socket()
+    #s.setsockopt(socket.SOL_SOCKET,socket.SO_SNDBUF,1000000)
+
+    server_ip = "127.0.0.1"
+    server_port = 7777
+
+    s.connect((server_ip,server_port))
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.setsockopt(socket.SOL_SOCKET,socket.SO_SNDBUF,1000000)
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
 
-server_ip = "127.0.0.1"
-server_port = 7777
-
-
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-
-while cap.isOpened():
+    while cap.isOpened():
+        ret, img = cap.read()
     
-	ret, img = cap.read()
- 
-	cv2.imshow('Img Client', img)
- 
-	ret, buffer = cv2.imencode(".jpg", img, [int(cv2.IMWRITE_JPEG_QUALITY),30])
- 
-	x_as_bytes = pickle.dumps(buffer)
+        cv2.imshow('Img Client', img)
     
-	s.sendto((x_as_bytes),(server_ip,server_port))
- 
-	if cv2.waitKey(5) & 0xFF == 27:
-		break
+        ret, buffer = cv2.imencode(".jpg", img, [int(cv2.IMWRITE_JPEG_QUALITY),30])
+    
+        x_as_bytes = pickle.dumps(buffer)
+        
+        s.sendto((x_as_bytes),(server_ip,server_port))
+    
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
 
 
-cv2.destroyAllWindows()
-cap.release()
+    cv2.destroyAllWindows()
+    cap.release()
